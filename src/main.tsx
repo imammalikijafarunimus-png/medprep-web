e0438524d4f21c0cab9e2b3e5501a56eaf4f86f4
null <null>
2026-01-21 06:56:04 +0700

    import speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App' // Gunakan .tsx agar lebih eksplisit (opsional tapi recommended)
 import './index.css' // <--- WAJIB pakai .css jika ini file style
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 // Tanda '!' di akhir menjamin element root tidak null (Sudah Benar)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
